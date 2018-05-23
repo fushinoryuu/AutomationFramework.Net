be5d14a1d1a21aba9fdb726a1af6e1f4e67f84f9
Christian Munoz <cmunoz@mastercontrol.com>
2018-05-23 16:04:40 -0600

    Created a simple interface for the config object.

---
+++ src/AutomationConfig/Interfaces/IAutomationConfig.cs
@@ -0,0 +1,12 @@
+﻿using AutomationConfig.Enums;
+using OperatingSystem = AutomationConfig.Enums.OperatingSystem;
+
+namespace AutomationConfig.Interfaces
+{
+    public interface IAutomationConfig
+    {
+        Browser TargetBrowser { get; set; }
+        OperatingSystem TargetOperatingSystem { get; set; }
+        string HubLocation { get; set; }
+    }
+}
