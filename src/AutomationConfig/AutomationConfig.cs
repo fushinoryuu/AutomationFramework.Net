0d1734ff8f7f13d00446bac193eaede75325e542
Christian Munoz <cmunoz@mastercontrol.com>
2018-05-23 16:07:02 -0600

    Created added a dependenci and created the config class.

---
+++ src/AutomationConfig/AutomationConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using AutomationConfig.Enums;
+using AutomationConfig.Interfaces;
+using OperatingSystem = AutomationConfig.Enums.OperatingSystem;
+
+namespace AutomationConfig
+{
+    public class AutomationConfig : IAutomationConfig
+    {
+        public Browser TargetBrowser { get; set; }
+        public OperatingSystem TargetOperatingSystem { get; set; }
+        public string HubLocation { get; set; }
+
+        public static IAutomationConfig DeserializeConfig(string fileName)
+        {
+            var file = File.ReadAllText(fileName);
+
+            return JsonConvert.DeserializeObject<AutomationConfig>(file);
+        }
+    }
+}
