cfe6c41ef6aeb8a2e79ae62909c9916c3c9f8717
Christian Munoz <cmunoz@mastercontrol.com>
2018-05-29 16:37:56 -0600

    Created a concrete class for the factory interface.

---
+++ src/PageObjects/Utilities/WebPageFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Driver.Interfaces;
+using PageObjects.Interfaces;
+using OpenQA.Selenium.Support.UI;
+
+namespace PageObjects.Utilities
+{
+    public class WebPageFactory : IWebPageFactory
+    {
+        private IAutomationDriver _driver;
+
+        public WebPageFactory(IAutomationDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public T Get<T>() where T : IWebPage, new()
+        {
+            var instance = new T
+            {
+                Driver = _driver,
+                Wait = new WebDriverWait(_driver, new TimeSpan(0, 0, 30))
+            };
+
+            instance.WaitForPageToLoad();
+
+            return instance;
+        }
+    }
+}
