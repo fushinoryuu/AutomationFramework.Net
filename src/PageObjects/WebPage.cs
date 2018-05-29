d63f8879629775a3329fed04f2d543b8cacc65bf
Christian Munoz <cmunoz@mastercontrol.com>
2018-05-29 16:38:29 -0600

    Created an abstract webpage class that all other page classes will have to inherit from.

---
+++ src/PageObjects/WebPage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using Driver.Interfaces;
+using PageObjects.Interfaces;
+using System.Collections.Generic;
+
+namespace PageObjects
+{
+    public abstract class WebPage : IWebPage
+    {
+        public IAutomationDriver Driver { get; set; }
+        public WebDriverWait Wait { get; set; }
+
+        public IWebElement FindElementBy(By by)
+        {
+            return Driver.FindElement(by);
+        }
+
+        public IList<IWebElement> FindElementsBy(By by)
+        {
+            return Driver.FindElements(by);
+        }
+
+        public abstract void WaitForPageToLoad();
+    }
+}
