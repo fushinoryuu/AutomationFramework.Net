8e1e6b56b7c55efc898167cd54068799aff28c36
Christian Munoz <cmunoz@mastercontrol.com>
2018-05-29 15:18:16 -0600

    Added a couple new interfaces for the page objects.

---
+++ src/PageObjects/Interfaces/IWebPageFactory.cs
@@ -0,0 +1,11 @@
+﻿using Driver.Interfaces;
+
+namespace PageObjects.Interfaces
+{
+    public interface IWebPageFactory
+    {
+        IAutomationDriver Driver { get; set; }
+
+        T Get<T>();
+    }
+}
