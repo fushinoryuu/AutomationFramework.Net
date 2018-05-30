83f9cbb7921b284285588434eb844082d3174a00
Christian Munoz <cmunoz@mastercontrol.com>
2018-05-30 09:59:05 -0600

    Updated the interface to allow for a custom wait when generating a new page object from the factory.

--- src/PageObjects/Interfaces/IWebPageFactory.cs
+++ src/PageObjects/Interfaces/IWebPageFactory.cs
@@ -1,7 +1,10 @@
-﻿namespace PageObjects.Interfaces
+﻿using System;
+
+namespace PageObjects.Interfaces
 {
     public interface IWebPageFactory
     {
         T Get<T>() where T : IWebPage, new();
+        T Get<T>(TimeSpan customWait) where T : IWebPage, new();
     }
 }
--- src/PageObjects/Utilities/WebPageFactory.cs
+++ src/PageObjects/Utilities/WebPageFactory.cs
@@ -26,5 +26,18 @@ namespace PageObjects.Utilities
 
             return instance;
         }
+
+        public T Get<T>(TimeSpan customWait) where T : IWebPage, new()
+        {
+            var instance = new T
+            {
+                Driver = _driver,
+                Wait = new WebDriverWait(_driver, customWait)
+            };
+
+            instance.WaitForPageToLoad();
+
+            return instance;
+        }
     }
 }
