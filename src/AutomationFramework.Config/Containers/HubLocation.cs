1a92d76a4782974c5e26b4d9b90ab095519ab979
Christian Munoz <cmunoz@mastercontrol.com>
2018-06-01 22:52:32 -0600

    Created a new class for hub locations in the config file.

---
+++ src/AutomationFramework.Config/Containers/HubLocation.cs
@@ -0,0 +1,10 @@
+﻿using AutomationFramework.Config.Enums;
+
+namespace AutomationFramework.Config.Containers
+{
+    public class HubLocation
+    {
+        public DriverLocation Location { get; set; }
+        public string Url { get; set; }
+    }
+}
