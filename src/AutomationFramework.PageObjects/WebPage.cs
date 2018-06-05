d5a89470ad12f14cd07a0c0e502bbfe20c758916
Christian Munoz <cmunoz@mastercontrol.com>
2018-06-05 13:13:29 -0600

    Made a change to the WaitForPageToLoad() method in the WebPage class so it doesn't have to be made on each page.

--- src/AutomationFramework.PageObjects/WebPage.cs
+++ src/AutomationFramework.PageObjects/WebPage.cs
@@ -1,6 +1,7 @@
-﻿using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
+﻿using System;
 using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using AutomationFramework.Driver.Interfaces;
 using AutomationFramework.PageObjects.Interfaces;
 
@@ -11,17 +12,24 @@ namespace AutomationFramework.PageObjects
         public IAutomationDriver Driver { get; set; }
         public WebDriverWait Wait { get; set; }
         public IWebPageFactory Factory { get; set; }
+        protected List<IWebElement> WebElements;
 
-        public IWebElement FindElementBy(By by)
+        protected WebPage() => WebElements = new List<IWebElement>();
+
+        public IWebElement FindElementBy(By by) => Driver.FindElement(by);
+
+        public IList<IWebElement> FindElementsBy(By by) => Driver.FindElements(by);
+
+        public virtual void WaitForPageToLoad()
         {
-            return Driver.FindElement(by);
-        }
+            const string errorMessage = "The 'WebElements' list is empty. You have to add web elements " +
+                "to the list that you wish to use to check if a page has loaded.";
 
-        public IList<IWebElement> FindElementsBy(By by)
-        {
-            return Driver.FindElements(by);
-        }
+            if (WebElements.Count < 1)
+                throw new InvalidOperationException(errorMessage);
 
-        public abstract void WaitForPageToLoad();
+            foreach (var element in WebElements)
+                Wait.Until(func => element.Displayed && element.Enabled);
+        }
     }
 }
