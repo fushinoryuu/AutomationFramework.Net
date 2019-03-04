e8fa03796109e6349f262b936662766fce07f4a5
Christian Munoz <christian.munoz@extendhealth.com>
2019-03-03 17:24:45 -0700

    Minor update to the hub and node scripts.

--- selenium/HUB_LaunchSeleniumGrid.bat
+++ selenium/HUB_LaunchSeleniumGrid.bat
@@ -5,4 +5,4 @@ TITLE Selenium Grid - HUB
 ECHO Starting the HUB...
 ECHO.
 CD /Selenium
-java -jar selenium-server-standalone-3.12.0.jar -role hub -hubConfig HUB_Config.json
+java -jar selenium-server-standalone-3.14.0.jar -role hub -hubConfig HUB_Config.json
--- selenium/NODE_LaunchSeleniumGrid.bat
+++ selenium/NODE_LaunchSeleniumGrid.bat
@@ -5,4 +5,4 @@ TITLE Selenium Grid - NODE
 ECHO Starting the NODE...
 ECHO.
 CD /Selenium
-java -jar selenium-server-standalone-3.12.0.jar -role node -nodeConfig NODE_Config.json
+java -jar selenium-server-standalone-3.14.0.jar -role node -nodeConfig NODE_Config.json
